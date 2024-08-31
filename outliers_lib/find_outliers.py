7e793035370e4040ae798cb22b14cf9d88a34215
Wanderer <a.shev4uk1975@gmail.com>
2024-08-31 18:36:12 +0300

    added function find_outliers_quantile

--- outliers_lib/find_outliers.py
+++ outliers_lib/find_outliers.py
@@ -59,3 +59,45 @@ def find_outliers_z_score(data, feature, left=3, right=3, log_scale=False):
     cleaned = data[(x > lower_bound) & (x < upper_bound)]
     return outliers, cleaned
 
+
+def find_outliers_quantile(data, feature, left=0.01, right=0.99):
+    """
+    Функция для поиска выбросов в данных на основе квантилей.
+
+    Параметры:
+    ----------
+    data : pandas.DataFrame
+        Входные данные в виде DataFrame, содержащие признак для анализа.
+        
+    feature : str
+        Имя столбца в DataFrame, по которому необходимо найти выбросы.
+        
+    left : float, optional, default=0.01
+        Левый квантиль для определения нижней границы выбросов.
+        
+    right : float, optional, default=0.99
+        Правый квантиль для определения верхней границы выбросов.
+
+    Возвращаемые значения:
+    ----------------------
+    outliers : pandas.DataFrame
+        DataFrame, содержащий строки с выбросами, которые находятся за пределами 
+        указанных квантилей.
+        
+    cleaned : pandas.DataFrame
+        DataFrame, содержащий строки без выбросов, находящиеся между указанными квантилями.
+
+    Пример использования:
+    ---------------------
+    outliers, cleaned = find_outliers_quantile(data, 'price', left=0.05, right=0.95)
+    """
+    x = data[feature]
+    lower_bound = x.quantile(left)
+    upper_bound = x.quantile(right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
+
+
+
+
